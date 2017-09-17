066bad6b08c8194bf0f8e16953dbfb05b15f7c0a
jonahh-yeti <jonah.husson1995@gmail.com>
2017-09-17 13:31:39 -0400

    initial commit

---
+++ project1/main.py
@@ -0,0 +1,25 @@
+import socket
+import ssl
+import sys
+
+port = sys.argv[1]
+ssl = sys.argv[2]
+host = sys.argv[3]
+nuid = sys.argv[4]
+
+s = socket.socket()
+
+if ssl:
+  s = ssl.wrap_socket(s, keyfile='domain.key', certfile='domain.crt')
+
+s.connect((host, port))
+
+s.sendall("cs3700fall2017 HELLO " + str(nuid) +"\n")
+msg = s.recv(256)
+
+while (msg.find("BYE") == -1):
+  print msg
+  ans = eval(msg[22:])
+  s.sendall("cs3700fall2017 " + str(ans) + "\n")
+  msg = s.recv(256)
+print msg[15:msg.find("BYE")-1]
