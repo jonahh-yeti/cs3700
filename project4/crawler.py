19ec1cd971bc96dfc546939c5dd88fb2ca509a3b
zackr <reiter.z@husky.neu.edu>
2017-11-11 16:22:12 -0500

    get csrf token from log in site

--- project4/crawler.py
+++ project4/crawler.py
@@ -5,6 +5,34 @@ import socket
 import urlparse
 import re
 import os
+from HTMLParser import HTMLParser
+
+
+# create a subclass and override the handler methods
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        if tag == 'input':
+            print attrs
+            newkeys = [i[0] for i in attrs]
+            print newkeys
+            try:
+                indexOfName = newkeys.index('name')
+                if indexOfName > 0:
+                    if attrs[indexOfName][1] == 'csrfmiddlewaretoken':
+                        token = attrs[newkeys.index('value')][1]
+                        print token
+            except ValueError:
+                print "whoops not found"
+
+    def handle_endtag(self, tag):
+        print "Encountered an end tag :", tag
+
+    def handle_data(self, data):
+        print "Encountered some data  :", data
+
+
+# instantiate the parser and fed it some HTML
+parser = MyHTMLParser()
 
 socket.setdefaulttimeout = 0.50
 os.environ['no_proxy'] = '127.0.0.1,localhost'
@@ -12,8 +40,10 @@ linkRegex = re.compile('<a\s*href=[\'|"](.*?)[\'"].*?>')
 CRLF = "\r\n\r\n"
 
 
+
 def GET(url):
     url = urlparse.urlparse(url)
+    print url
     path = url.path
     if path == "":
         path = "/"
@@ -41,13 +71,23 @@ def GET(url):
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     #s.setblocking(0)
     s.connect((HOST, PORT))
-    s.send("GET / HTTP/1.0%s" % (CRLF))
+    message = "GET %s?%s HTTP/1.1%shost: %s%s" % (path, url.query, '\r\n', HOST, CRLF)
+    print url.path
+    print url.params
+    print url.query
+    print url.fragment
+    print message
+    s.send(message)
     data = (s.recv(1000000))
     print data
+
+    parser.feed(data)
+
+
     # https://docs.python.org/2/howto/sockets.html#disconnecting
     s.shutdown(1)
     s.close()
     print 'Received', repr(data)
 
 
-GET('http://fring.ccs.neu.edu')
+GET('http://fring.ccs.neu.edu/accounts/login/?next=/fakebook/')
